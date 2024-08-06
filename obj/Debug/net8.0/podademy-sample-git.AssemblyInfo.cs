26593858cb4d8a65519e6b6088bda4ac8dd38920
peter_m_holman <peter_m_holman@hotmail.com>
2024-08-06 14:07:03 +1000

    m

--- obj/Debug/net8.0/podademy-sample-git.AssemblyInfo.cs
+++ obj/Debug/net8.0/podademy-sample-git.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("podademy-sample-git")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12eb79438e008a525a391889f57ced0fdd0577ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("podademy-sample-git")]
 [assembly: System.Reflection.AssemblyTitleAttribute("podademy-sample-git")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
